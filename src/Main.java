22b0b07234cf197741e4bb6206a20fb937e48e05
ImAStudentAtUniversity <215722@edu.p.lodz.pl>
2021-04-12 20:50:06 +0200

    Zadanie 1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Nazwa grupy: Chrum, ID leadera: Tomek008, rola: developer");
-        System.out.println("Filip Amro - Github ID: wiezawieza10");
-        System.out.println("Tomasz Nykiel - Github ID: Tomek008");
+        System.out.println("Patryk Gajda - Github ID: ImAStudentAtUniversity");
     }
 }
