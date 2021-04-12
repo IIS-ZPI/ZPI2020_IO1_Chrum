74405b8e7bd0f14beaced1a0b752a7ca38d74451
Jakub <kuba.kaniewski123@gmail.com>
2021-04-12 22:10:43 +0200

    Zadanie_2.1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Nazwa grupy: Chrum, ID testera: Blemm13, rola: tester");
+        System.out.println("Nazwa grupy: Chrum, ID leadera: Tomek008, rola: developer");
+        System.out.println("Filip Amro - Github ID: wiezawieza10");
+        System.out.println("Tomasz Nykiel - Github ID: Tomek008");
+		System.out.println("Patryk Gajda - Github ID: ImAStudentAtUniversity");
+		System.out.println("Jakub Kaniewski - Github ID: Blemm13");
     }
 }
