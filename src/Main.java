00567f3f8e085f0c30f496c444157a7de9f33a0e
Tomek008 <tomasz.nykiel123@gmail.com>
2021-04-12 19:57:16 +0200

    Zadanie 2

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Nazwa grupy: Chrum, ID leadera: Tomek008, rola: developer");
         System.out.println("Filip Amro - Github ID: wiezawieza10");
+        System.out.println("Tomasz Nykiel - Github ID: Tomek008");
     }
 }
