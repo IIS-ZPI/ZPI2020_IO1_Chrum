073a76c74d2580de3fc779cf56f7f8e91c32925b
Tomek008 <tomasz.nykiel123@gmail.com>
2021-04-13 11:01:23 +0200

    Addition implementation

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
-public class Main {
+public class Main implements IArithmeticsAdd{
     public static void main(String[] args) {
         System.out.println("Nazwa grupy: Chrum, ID leadera: Tomek008, rola: developer");
         System.out.println("Filip Amro - Github ID: wiezawieza10");
         System.out.println("Tomasz Nykiel - Github ID: Tomek008");
         System.out.println("Patryk Gajda - Github ID: ImAStudentAtUniversity");
         System.out.println("Jakub Kaniewski - Github ID: Blemm13 ");
+    }
+
+    @Override
+    public double Addition(double A, double B) {
+        return A+B;
     }
 }
