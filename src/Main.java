14ee77a78ff1b33deea37e8105ed2ad7dc5e1f4d
Jakub <kuba.kaniewski123@gmail.com>
2021-04-13 11:06:25 +0200

    Added Division interface implementation

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,26 @@
-public class Main {
+public class Main implements IArithmeticsDiv{
     public static void main(String[] args) {
         System.out.println("Nazwa grupy: Chrum, ID leadera: Tomek008, rola: developer");
         System.out.println("Filip Amro - Github ID: wiezawieza10");
         System.out.println("Tomasz Nykiel - Github ID: Tomek008");
         System.out.println("Patryk Gajda - Github ID: ImAStudentAtUniversity");
         System.out.println("Jakub Kaniewski - Github ID: Blemm13 ");
+    }
+
+    @Override
+    public double Division(double A, double B) {
+
+        double threshold = 0.000001;
+        if( B >= -threshold && B <= threshold ){
+            return A/B;
+        }
+        else {
+            try {
+                throw new Exception("Cannot divide by 0!");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return 0;
+        }
     }
 }
